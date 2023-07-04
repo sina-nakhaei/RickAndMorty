9fa2fc610eecd5a1ea44d7dbe80ddd5c5ea67661
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-07-04 20:45:37 +0330

    feat: <NetworkModule> provide http HttpClient

---
+++ app/src/main/java/com/example/rickandmorty/core/module/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.example.rickandmorty.core.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val baseUrl: String = ""
+
+    @Provides
+    @Singleton
+    fun provideKtorClient(): HttpClient {
+        return HttpClient(OkHttp) {
+            expectSuccess = true
+            defaultRequest {
+                host = baseUrl
+                url {
+                    protocol = URLProtocol.HTTPS
+                }
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+}
