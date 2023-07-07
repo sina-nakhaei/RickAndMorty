74a3d8d67bd15f60332688822d8b36cd35672df3
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-07-07 22:43:33 +0330

    feat: <BasePaginatedResponse>

---
+++ app/src/main/java/com/example/rickandmorty/core/model/BasePaginatedResponse.kt
@@ -0,0 +1,28 @@
+package com.example.rickandmorty.core.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BasePaginatedResponse<T>(
+    @SerialName("info")
+    val info: Info,
+
+    @SerialName("results")
+    val results: List<T>
+)
+
+@Serializable
+data class Info(
+    @SerialName("count")
+    val count: Int,
+
+    @SerialName("pages")
+    val pages: Int,
+
+    @SerialName("next")
+    val next: String?,
+
+    @SerialName("prev")
+    val prev: String?
+)
