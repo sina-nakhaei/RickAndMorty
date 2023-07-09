03acead36cb791036d756cee7ce25dca59c9a31a
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-07-09 20:02:19 +0330

    update: <Result> implement essential extensions

--- app/src/main/java/com/example/rickandmorty/core/model/Result.kt
+++ app/src/main/java/com/example/rickandmorty/core/model/Result.kt
@@ -8,7 +8,30 @@ sealed interface Result<out T> {
     data class Error(
         val message: String? = null,
         val type: String
-    ) : Result<Unit>
+    ) : Result<Nothing>
 
-    object Loading : Result<Unit>
+    object Loading : Result<Nothing>
+}
+
+inline fun <T> Result<T>.extractResult(
+    block: (data: T?, error: Result.Error?, loading: Result.Loading?) -> Unit
+) {
+    when (this) {
+        is Result.Loading -> block(null, null, this)
+        is Result.Error -> block(null, this, null)
+        is Result.Success -> block(this.data, null, null)
+    }
+}
+
+fun <T> Result<T>.extractData(): T? =
+    if (this is Result.Success)
+        this.data
+    else null
+
+inline fun <T, R> Result<T>.map(crossinline transform: (value: T) -> R): Result<R> {
+    return when (this) {
+        is Result.Success -> Result.Success(transform(data!!))
+        is Result.Error -> this
+        is Result.Loading -> this
+    }
 }
