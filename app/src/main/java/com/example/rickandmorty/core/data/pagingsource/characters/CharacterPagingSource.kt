5ccf4aa18f499a70938c4824195e95aab0a8a8b0
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-07-27 12:02:22 +0330

    feat: <CharacterPagingSource>

---
+++ app/src/main/java/com/example/rickandmorty/core/data/pagingsource/characters/CharacterPagingSource.kt
@@ -0,0 +1,50 @@
+package com.example.rickandmorty.core.data.pagingsource.characters
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.rickandmorty.core.data.remotedatasource.characters.CharacterRemoteDataSource
+import com.example.rickandmorty.core.data.remotedatasource.characters.model.CharacterResponse
+import com.example.rickandmorty.core.model.NetworkResult
+import com.example.rickandmorty.core.model.extractData
+import kotlinx.coroutines.flow.last
+
+class CharacterPagingSource(
+    private val dataSource: CharacterRemoteDataSource
+) : PagingSource<Int, CharacterResponse>() {
+    override fun getRefreshKey(state: PagingState<Int, CharacterResponse>): Int? =
+        state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CharacterResponse> {
+        val page = params.key ?: 1
+
+        return when (val result = dataSource.getAllCharacters(page).last()) {
+            is NetworkResult.Error -> {
+                //TODO: handle errors
+                LoadResult.Error(Exception(""))
+            }
+
+            is NetworkResult.Success -> {
+                val next = result.data?.info?.next
+                val prev = result.data?.info?.prev
+
+                LoadResult.Page(
+                    result.extractData()?.results ?: emptyList(),
+                    prev?.extractPageNumber(),
+                    next?.extractPageNumber()
+                )
+            }
+            //TODO: handle errors
+            else -> LoadResult.Error(Exception(""))
+        }
+    }
+}
+
+private fun String.extractPageNumber(): Int? {
+    val regex = Regex("page=(\\d+)")
+    val result = regex.find(this)
+
+    return result?.groupValues?.get(1)?.toIntOrNull()
+}
