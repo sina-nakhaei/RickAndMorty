1af906b09eb4cc560cbc77e21f7a0498bd45b564
Sina Nakhaei <36819672+sina-nakhaei@users.noreply.github.com>
2023-07-27 17:16:46 +0330

    feat: <NetworkStatus>

---
+++ app/src/main/java/com/example/rickandmorty/core/network/NetworkStatus.kt
@@ -0,0 +1,49 @@
+package com.example.rickandmorty.core.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class NetworkStatus @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    val isOnline: Boolean
+        get() = isNetworkAvailable()
+
+    val isOffline: Boolean
+        get() = !isNetworkAvailable()
+
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+    }
+}
